78e9e12204096cff0f2bfdb054261ff6795286d8
Diffa <diffadwi1@gmail.com>
2021-04-30 02:39:22 +0700

    final app

--- Togle App/ViewController.swift
+++ Togle App/ViewController.swift
@@ -14,10 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var fogleSegmentationControl: UISegmentedControl!
     @IBOutlet weak var pointLabel: UIButton!
     
+    @IBOutlet weak var labelEmptyView: UILabel!
+    @IBOutlet weak var imageEmptyView: UIImageView!
     var fogleData : [NSManagedObject] = []
     let statusSegmnetation : [FogleStatus] = [.todo,.uncompleted,.completed]
     private var db : FogleDB?
     var badgesData : NSManagedObject?
+    
+    var listData : [FogleModel] = []
+    
+    var labelEmpty : [String] = ["There are no tasks yet.\nHow about making your own tasks?", "There are no uncompleted tasks yet.\nHow about making your own tasks or start your task you have added?", "There are no completed tasks yet.\nHow about making your own tasks or start your task you have added?"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +46,7 @@ class ViewController: UIViewController {
             }
         )
         self.reloadData()
+        
     }
         
     func setTable(){
@@ -55,22 +62,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func didChangeSegment(_ sender: UISegmentedControl) {
-        
-        if sender.selectedSegmentIndex == 0 {
-            fogleData = db?.fetchDataByStatus(status: .todo) ?? []
-            
-            itemTableViews.reloadData()
-        }
-        else if sender.selectedSegmentIndex == 1 {
-            fogleData = db?.fetchDataByStatus(status: .uncompleted) ?? []
-            itemTableViews.reloadData()
-        }
-        else if sender.selectedSegmentIndex == 2 {
-            fogleData = db?.fetchDataByStatus(status: .completed) ?? []
-            
-            itemTableViews.reloadData()
-        }
-       
+        reloadData()
     }
     
     @IBAction func actionAddButton(_ sender: Any) {
@@ -84,7 +76,6 @@ class ViewController: UIViewController {
         let vc = storyboard?.instantiateViewController(identifier: "BadgesViewController") as! BadgesViewController
         
         vc.badges = createInstanceBadges(data: badgesData!)
-        //        self.present(vc, animated: true, completion: nil)
         self.navigationController?.pushViewController(vc, animated: true)
 
     }
@@ -109,28 +100,37 @@ extension ViewController: UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let itemData = fogleData[indexPath.row]
-//        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "extendItemDataTable", for: indexPath) as! ExtendItemTableViewCell
-        cell.backgroundColor = UIColor.white
-        cell.updateUI(fogle: itemData)
+        let data = listData[indexPath.row]
         
-        return cell
+        print(data.highlight)
+        if data.highlight {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "extendItemDataTable", for: indexPath) as! ExtendItemTableViewCell
+            cell.backgroundColor = UIColor.white
+            cell.updateUI(fogle: itemData)
+            cell.startCallback = {
+                self.openTimerScreen(badgesData: self.badgesData!, fogleData: itemData)
+            }
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "itemDataTable", for: indexPath) as! ItemTableViewCell
+            cell.backgroundColor = UIColor.white
+            cell.updateUI(fogle: itemData)
 
-//        if true {
-//        } else {
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "itemDataTable", for: indexPath) as! ItemTableViewCell
-//            cell.backgroundColor = UIColor.white
-//            cell.updateUI(fogle: itemData)
-//
-//            return cell
-//        }
+            return cell
+        }
         
     }
 }
 
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return tableView.frame.height/6
+        let data = listData[indexPath.row]
+        
+        if data.highlight {
+            return tableView.frame.height/5
+        }
+        
+        return tableView.frame.height/8
     }
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -184,14 +184,9 @@ extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let data = fogleData[indexPath.row]
-        
-        let vc = storyboard?.instantiateViewController(identifier: "TimeScreenViewController") as! TimeScreenViewController
-        vc.mainScreenProtocol = self
-        vc.fogleModel = createFogleModel(data: data)
-        vc.badgesModel = createInstanceBadges(data: badgesData!)
-        present(vc, animated: true, completion: nil)
-        
+        let itemData = listData[indexPath.row]
+        itemData.highlight.toggle()
+        tableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -206,16 +201,40 @@ extension ViewController: UITableViewDelegate {
 }
 
 extension ViewController : MainScreenProtocol {
+    func openTimerScreen(badgesData: NSManagedObject, fogleData: NSManagedObject) {
+        let vc = storyboard?.instantiateViewController(identifier: "TimeScreenViewController") as! TimeScreenViewController
+        vc.mainScreenProtocol = self
+        vc.fogleModel = createFogleModel(data: fogleData)
+        vc.badgesModel = createInstanceBadges(data: badgesData)
+        present(vc, animated: true, completion: nil)
+    }
+    
     func changeStatusTask(fogleModel: FogleModel) {
         db?.editFogleData(fogleModel: fogleModel)
         reloadData()
     }
     
     func reloadData() {
-        
+        listData = []
         fogleData = db?.fetchDataByStatus(status: statusSegmnetation[fogleSegmentationControl.selectedSegmentIndex]) ?? []
+        
+        for data in fogleData {
+            listData.append(createFogleModel(data: data))
+        }
+        
         badgesData = db?.fetchBadgesData()
         
+        if !fogleData.isEmpty {
+            itemTableViews.isHidden = false
+            labelEmptyView.isHidden = true
+            labelEmptyView.text = labelEmpty[fogleSegmentationControl.selectedSegmentIndex]
+            imageEmptyView.isHidden = true
+        } else {
+            itemTableViews.isHidden = true
+            labelEmptyView.isHidden = false
+            imageEmptyView.isHidden = false
+        }
+
         let point = (badgesData?.value(forKey: "point") as? Int64) ?? 0
         pointLabel.setTitle("🏆 \(point)", for: .normal)
         itemTableViews.reloadData()
